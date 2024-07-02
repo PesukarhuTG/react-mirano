86763a71172f1755b20433c1063a9e166b9a6cc6
PesukarhuTG <ganzhinovat@yandex.ru>
2024-07-03 00:43:26 +0300

    feat: add Filter component and transfer filter.scss and choices.scss to Filter folder

--- src/modules/index.js
+++ src/modules/index.js
@@ -5,3 +5,4 @@ export { default as Cart } from './Cart/Cart';
 export { default as Card } from './Card/Card';
 export { default as CartItem } from './CartItem/CartItem';
 export { default as Hero } from './Hero/Hero';
+export { default as Filter } from './Filter/Filter';

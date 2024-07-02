ac2319f4541a9bedc891000dd4fcd87cf3b34877
PesukarhuTG <ganzhinovat@yandex.ru>
2024-07-02 18:43:23 +0300

    feat: add components Header, Footer, Goods and Cart

---
+++ src/modules/index.js
@@ -0,0 +1,4 @@
+export { default as Footer } from './Footer/Footer';
+export { default as Header } from './Header/Header';
+export { default as Goods } from './Goods/Goods';
+export { default as Cart } from './Cart/Cart';

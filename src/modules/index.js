e1c5c2b4d2e3a47b0ae72eb7a4d26b1572e3adee
PesukarhuTG <ganzhinovat@yandex.ru>
2024-07-03 00:37:04 +0300

    feat: add Hero component and transfer hero.scss to Hero folder

--- src/modules/index.js
+++ src/modules/index.js
@@ -4,3 +4,4 @@ export { default as Goods } from './Goods/Goods';
 export { default as Cart } from './Cart/Cart';
 export { default as Card } from './Card/Card';
 export { default as CartItem } from './CartItem/CartItem';
+export { default as Hero } from './Hero/Hero';

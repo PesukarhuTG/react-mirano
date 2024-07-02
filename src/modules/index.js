87cc4e6da7c27206719f700833107b11304313b5
PesukarhuTG <ganzhinovat@yandex.ru>
2024-07-03 00:49:45 +0300

    feat: add Subscribe component and transfer subscribe.scss to Subscribe folder

--- src/modules/index.js
+++ src/modules/index.js
@@ -6,3 +6,4 @@ export { default as Card } from './Card/Card';
 export { default as CartItem } from './CartItem/CartItem';
 export { default as Hero } from './Hero/Hero';
 export { default as Filter } from './Filter/Filter';
+export { default as Subscribe } from './Subscribe/Subscribe';

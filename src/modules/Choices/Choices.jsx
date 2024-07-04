ca2efbbe5a33ed3267b1f6cfda79cc51a23efdf2
PesukarhuTG <ganzhinovat@yandex.ru>
2024-07-04 21:17:39 +0300

    feat: add useState for Choices component

--- src/modules/Choices/Choices.jsx
+++ src/modules/Choices/Choices.jsx
@@ -1,13 +1,24 @@
 import style from './Choices.module.scss';
+import { useState } from 'react';
 
 const Choices = ({ btnLabel, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen((oldIsOpen) => !oldIsOpen);
+  };
+
   return (
     <div className={style.choices}>
-      <button className={style.choices__btn} type="button">
+      <button
+        className={style.choices__btn}
+        type="button"
+        onClick={handleToggle}
+      >
         {btnLabel}
       </button>
 
-      <div className={style.choices__box}>{children}</div>
+      {isOpen && <div className={style.choices__box}>{children}</div>}
     </div>
   );
 };

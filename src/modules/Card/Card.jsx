44b9be1eb2c15985ab35773d9ec390cffd9c8877
PesukarhuTG <ganzhinovat@yandex.ru>
2024-07-07 22:18:03 +0300

    feat: change the price on button into text when we use hover in Card component

--- src/modules/Card/Card.jsx
+++ src/modules/Card/Card.jsx
@@ -2,9 +2,11 @@ import { useDispatch } from 'react-redux';
 import style from './Card.module.scss';
 import cn from 'classnames';
 import { addItemToCart } from '../../redux/cartSlice';
+import { useState } from 'react';
 
 const Card = ({ id, img, title, price, dateDelivery }) => {
   const dispatch = useDispatch();
+  const [isHover, setIsHover] = useState(false);
 
   const handleAddToCart = () => {
     dispatch(addItemToCart({ id, img, title, price }));
@@ -17,8 +19,13 @@ const Card = ({ id, img, title, price, dateDelivery }) => {
         <h3 className={style.title}>{title}</h3>
         <div className={style.footer}>
           <p className={style.dateDelivery}>{dateDelivery}</p>
-          <button className={style.button} onClick={handleAddToCart}>
-            {price}&nbsp;₽
+          <button
+            className={style.button}
+            onClick={handleAddToCart}
+            onMouseOver={() => setIsHover((oldIsHover) => !oldIsHover)}
+            onMouseOut={() => setIsHover((oldIsHover) => !oldIsHover)}
+          >
+            {!isHover ? `${price} ₽` : 'В корзину'}
           </button>
         </div>
       </div>
